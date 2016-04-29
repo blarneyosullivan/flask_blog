9ad203dabbb9b4a06cc056a215726fd168da102f
blarneyosullivanbloggs <blarneyosullivanbloggs@gmail.com>
2016-04-29 13:37:24 +0000

    author model

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+#from flask.ext.sqlalchemy import SQLALCHEMY
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('settings')
+#db = SQLALCHEMY(app)
+db = SQLAlchemy(app)
 
-from home import views
+from blog import views
+from author import views
--- settings.py
+++ settings.py
@@ -1,2 +1,11 @@
+import os
+
 SECRET_KEY = "-\x85\x99Q5\x89\xe7@\x06\xd3\xa6=G\xcf k\xce6'\x16\xd6\xbb\xe6q"
 DEBUG = True
+DB_USERNAME = 'blarneyosullivan'
+DB_PASSWORD = ''
+BLOG_DATABASENAME = 'blog'
+DB_HOST = os.getenv('IP','0.0.0.0')
+DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASENAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
